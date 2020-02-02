a6ba6c073706d0c6bfe8a4c095163a3eed9fa3a6
iko <ilyakooo0@gmail.com>
2020-02-02 17:25:35 +0300

    Added readme

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 for file in *.md
 do
   base=$(basename $file .md)
-  pandoc -i $file -t revealjs -s --highlight-style=breezeDark --include-in-header=style.html >"$base.html"
+  if [base != "README"]
+  then
+    pandoc -i $file -t revealjs -s --highlight-style=breezeDark --include-in-header=style.html >"$base.html"
+  fi
 done
